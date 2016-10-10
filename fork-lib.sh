26daf2dac4737b68aeff722efa429bf6ace93b98
D. Andrei Măceș <dmaces@nd.edu>
2016-10-10 19:07:32 -0400

    fork-lib: Create library for use with M, L, and K

---
+++ fork-lib.sh
@@ -0,0 +1,59 @@
+#!/bin/sh
+
+FETCH=https://android.googlesource.com; CANARY=build
+SAUCE=~/android/$BRANCH; cd $SAUCE	# or whatever location
+
+git -C $CANARY fetch -q $FETCH/platform/$CANARY $TRACK && \
+NEWTAG=`git -C $CANARY describe --tag FETCH_HEAD`
+
+findtags() {
+	OLDTAG=`git -C $1 describe --tag --abbrev=0 $BRANCH`
+}
+
+findtags $CANARY
+
+if [ "$NEWTAG" = "$OLDTAG" ]; then
+	read -p "Seems like $CANARY is onto latest tag ($NEWTAG). Continue? (y/N) " prompt
+	if [ "$prompt" != "y" ]; then
+		exit 0
+	fi
+else
+	read -p "Found $OLDTAG in $CANARY; newest is $NEWTAG. Continue? (y/N) " prompt
+	if [ "$prompt" != "y" ]; then
+		exit 0
+	fi
+fi
+
+forkrebase() {
+	for r in $AOSP_FORKS; do
+		findtags $r
+		if [ "$NEWTAG" != "$OLDTAG" ]; then
+			echo "Rebasing $r onto $NEWTAG (from $OLDTAG)"
+			git -C $r rebase --onto $NEWTAG $OLDTAG $BRANCH
+		else
+			echo "Seems like $r is rebased onto latest tag already."
+		fi
+	done
+}
+
+#GERRIT=gerrit.unlegacy-android.cf:29418; $USER=???
+#Actually, better create a remote in ~/.ssh/config:
+#Host ul
+#    Port 29418
+#    User ???
+#    HostName gerrit.unlegacy-android.cf
+
+PJROOT=Unlegacy-Android; #UA=ssh://$USER@$GERRIT/$PJROOT
+
+forkpush() {
+	for r in $AOSP_FORKS; do
+		f=android_`echo $r| tr / _`; echo $f
+		git -C $r push ul:$PJROOT/$f $BRANCH:refs/heads/$BRANCH -f
+	done
+}
+
+forkstatus() {
+	for r in $AOSP_FORKS; do
+		git -C $r status -bs; echo $r
+	done
+}
--- n-forked.sh
+++ n-forked.sh
@@ -1,31 +1,6 @@
 #!/bin/sh
 
-TRACK=nougat-mr0.5-release
-BRANCH=aosp-7.0; CANARY=build
-FETCH=https://android.googlesource.com
-
-SAUCE=~/android/$BRANCH; cd $SAUCE	# or whatever location
-
-git -C $CANARY fetch -q $FETCH/platform/$CANARY $TRACK && \
-NEWTAG=`git -C $CANARY describe --tag FETCH_HEAD`
-
-findtags() {
-	OLDTAG=`git -C $1 describe --tag --abbrev=0 $BRANCH`
-}
-
-findtags $CANARY
-
-if [ "$NEWTAG" = "$OLDTAG" ]; then
-	read -p "Seems like $CANARY is onto latest tag ($NEWTAG). Continue? (y/N) " prompt
-	if [ "$prompt" != "y" ]; then
-		exit 0
-	fi
-else
-	read -p "Found $OLDTAG in $CANARY; newest is $NEWTAG. Continue? (y/N) " prompt
-	if [ "$prompt" != "y" ]; then
-		exit 0
-	fi
-fi
+BRANCH=aosp-7.0; TRACK=nougat-mr0.5-release
 
 AOSP_FORKS="build bionic system/core
 			frameworks/av frameworks/base frameworks/native
@@ -34,38 +9,6 @@ AOSP_FORKS="build bionic system/core
 			hardware/qcom/gps hardware/qcom/keymaster
 			hardware/qcom/media hardware/qcom/wlan"
 
-forkrebase() {
-	for r in $AOSP_FORKS; do
-		findtags $r
-		if [ "$NEWTAG" != "$OLDTAG" ]; then
-			echo "Rebasing $r onto $NEWTAG (from $OLDTAG)"
-			git -C $r rebase --onto $NEWTAG $OLDTAG $BRANCH
-		else
-			echo "Seems like $r is rebased onto latest tag already."
-		fi
-	done
-}
+. `dirname $0`/fork-lib.sh
 
 forkrebase
-
-#GERRIT=gerrit.unlegacy-android.cf:29418; $USER=???
-#Actually, better create a remote in ~/.ssh/config:
-#Host ul
-#    Port 29418
-#    User ???
-#    HostName gerrit.unlegacy-android.cf
-
-PJROOT=Unlegacy-Android; #UA=ssh://$USER@$GERRIT/$PJROOT
-
-forkpush() {
-	for r in $AOSP_FORKS; do
-		f=android_`echo $r| tr / _`; echo $f
-		git -C $r push ul:$PJROOT/$f $BRANCH:refs/heads/$BRANCH -f
-	done
-}
-
-forkstatus() {
-	for r in $AOSP_FORKS; do
-		git -C $r status -bs; echo $r
-	done
-}
