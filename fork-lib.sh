c8f88135e8290b2bb0fd37d02ec25d9df5f2354a
D. Andrei Măceș <dmaces@nd.edu>
2016-10-15 17:23:38 -0400

    fork-lib: Only mark modified forks for pushing
    Change-Id: Iffcde618c0401cc3eb07786fea05fe66a5e4de8f

--- fork-lib.sh
+++ fork-lib.sh
@@ -5,11 +5,11 @@ UA=Unlegacy-Android; GH=https://github.com; CANARY=build
 
 cd $SAUCE && echo "Using $SAUCE" || echo "Using $PWD"
 
-errout() { echo "$1"; exit 1; }
+mexit() { echo "$1"; exit $2; }
 
 fetchtags() {
 	git -C $1 fetch -qt $AOSP/platform/$1 $TRACK || \
-	errout "Failed to get upstream tags in $1; exiting!"
+	mexit "Failed to get upstream tags in $1; exiting!" 1
 }
 
 fetchtags $CANARY && NEWTAG=`git -C $CANARY describe --tag FETCH_HEAD`
@@ -19,7 +19,7 @@ atoua() { echo android_`echo $1| tr / _`; }
 forkcout() {
 	git -C $1 fetch -qt $GH/$UA/`atoua $1` $BRANCH && \
 	git -C $1 checkout -q FETCH_HEAD || \
-	errout "Failed to get $BRANCH branch in $1; exiting!"
+	mexit "Failed to get $BRANCH branch in $1; exiting!" 1
 }
 
 oldtag() { forkcout $1 && OLDTAG=`git -C $1 describe --tag --abbrev=0 FETCH_HEAD`; }
@@ -32,13 +32,16 @@ prompterr() { read -p "$1 Continue? ($2/N) " prompt; [ "$prompt" = "$2" ] || exi
 prompterr "Found $OLDTAG in $CANARY; newest is $NEWTAG." "y" || \
 prompterr "Seems like $CANARY is already onto latest tag ($NEWTAG)." "y"
 
+PUSH_FORKS=""
+
 rebaseforks() {
 	for r in $AOSP_FORKS; do
 		fetchtags $r; oldtag $r
 		if [ "$NEWTAG" != "$OLDTAG" ]; then
 			echo "Rebasing $r onto $NEWTAG (from $OLDTAG)"
 			git -C $r rebase --onto $NEWTAG $OLDTAG HEAD || \
-			errout "Rebase failed in $r; please fix!"
+			mexit "Rebase failed in $r; please fix!" 1 && \
+			PUSH_FORKS="$PUSH_FORKS $r"
 		else
 			echo "Seems like $r is rebased onto latest tag already."
 		fi
@@ -55,8 +58,9 @@ rebaseforks() {
 GERRIT=uag:$UA
 
 pushforks() {
+	[ "$PUSH_FORKS" = "" ] && mexit "Nothing new to push!" 0 || \
 	prompterr "Really force push rebased HEADs to Gerrit?" "yes"
-	for r in $AOSP_FORKS; do
+	for r in $PUSH_FORKS; do
 		git -C $r push $GERRIT/`atoua $r` HEAD:refs/heads/$BRANCH -f
 	done
 }
