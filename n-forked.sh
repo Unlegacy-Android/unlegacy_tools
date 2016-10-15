051a0a3784a73e67b8485e9ffe27f8678db2d122
D. Andrei Măceș <dmaces@nd.edu>
2016-10-15 16:10:06 -0400

    n-forked: Cannot import frameworks/base yet

--- n-forked.sh
+++ n-forked.sh
@@ -3,7 +3,7 @@
 BRANCH=aosp-7.0; TRACK=nougat-mr0.5-release
 
 AOSP_FORKS="build bionic system/core
-			frameworks/av frameworks/base frameworks/native
+			frameworks/av frameworks/native
 			hardware/broadcom/wlan hardware/qcom/audio
 			hardware/qcom/bt hardware/qcom/display
 			hardware/qcom/gps hardware/qcom/keymaster
