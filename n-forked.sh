945973dfbcb4d3207ab911bb5f9ebaf9a7636187
D. Andrei Măceș <dmaces@nd.edu>
2016-11-08 12:01:35 -0500

    Revert "n-forked: Cannot import frameworks/base yet"
    This reverts commit 051a0a3784a73e67b8485e9ffe27f8678db2d122.
    
    Change-Id: I60c9e79e6d98cc1a352f2ce100e9006c1a17d8a7

--- n-forked.sh
+++ n-forked.sh
@@ -3,7 +3,7 @@
 FALLBR=aosp-7.0; BRANCH=aosp-7.1; TRACK=nougat-dr1-release
 
 AOSP_FORKS="build bionic system/core
-			frameworks/av frameworks/native
+			frameworks/av frameworks/base frameworks/native
 			hardware/broadcom/wlan hardware/qcom/audio
 			hardware/qcom/bt hardware/qcom/display
 			hardware/qcom/gps hardware/qcom/keymaster
