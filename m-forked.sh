fd7d3d3bde3730f504bae2fa026415870942c8d7
D. Andrei Măceș <dmaces@nd.edu>
2017-03-23 10:02:42 -0400

    m-forked: Add frameworks/base to list
    Change-Id: Ic1fb36090cb35214a45f259f9a78d314bc4f6a9a

--- m-forked.sh
+++ m-forked.sh
@@ -2,7 +2,8 @@
 
 BRANCH=aosp-6.0; TRACK=marshmallow-mr2-release
 
-AOSP_FORKS="build system/core frameworks/av frameworks/native
+AOSP_FORKS="build system/core
+			frameworks/av frameworks/base frameworks/native
 			packages/apps/Camera2"
 
 . `dirname $0`/fork-lib.sh
