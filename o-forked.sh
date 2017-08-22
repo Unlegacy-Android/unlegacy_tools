bf37f152bd2532b0622743a04dee3eee0cca266f
D. Andrei Măceș <dmaces@nd.edu>
2017-08-22 16:13:51 +0300

    o-forked: Add support for & track oreo-release
    Change-Id: I6c6aebafd81f92bfca4bba415a1501833214f756

---
+++ o-forked.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+BRANCH=aosp-8.0; TRACK=oreo-release
+
+AOSP_FORKS="build/make build/soong bionic
+	system/bt system/core system/sepolicy
+	frameworks/av frameworks/base frameworks/native
+	hardware/broadcom/wlan hardware/qcom/audio
+	hardware/qcom/bt hardware/qcom/display
+	hardware/qcom/gps hardware/qcom/keymaster
+	hardware/qcom/media"
+
+. `dirname $0`/fork-lib.sh
+
+rebaseforks && pushforks
