0d04b3e66e40de29e9d7dc0dfdf35f5eeb0b84de
D. Andrei Măceș <dmaces@nd.edu>
2017-09-17 06:36:06 +0300

    m/n-forked: Clean up stray tabs in fork lists
    Change-Id: Iaee2ff29b85331307a461d2ca180861f9d86d3ba

--- m-forked.sh
+++ m-forked.sh
@@ -3,8 +3,8 @@
 BRANCH=aosp-6.0; TRACK=marshmallow-mr2-release
 
 AOSP_FORKS="build system/core
-			frameworks/av frameworks/base frameworks/native
-			packages/apps/Camera2"
+	frameworks/av frameworks/base frameworks/native
+	packages/apps/Camera2"
 
 . `dirname $0`/fork-lib.sh
 
--- n-forked.sh
+++ n-forked.sh
@@ -3,11 +3,11 @@
 FALLBR=aosp-7.0; BRANCH=aosp-7.1; TRACK=nougat-mr2.2-release
 
 AOSP_FORKS="build bionic system/core system/sepolicy
-			frameworks/av frameworks/base frameworks/native
-			hardware/broadcom/wlan hardware/qcom/audio
-			hardware/qcom/bt hardware/qcom/display
-			hardware/qcom/gps hardware/qcom/keymaster
-			hardware/qcom/media"
+	frameworks/av frameworks/base frameworks/native
+	hardware/broadcom/wlan hardware/qcom/audio
+	hardware/qcom/bt hardware/qcom/display
+	hardware/qcom/gps hardware/qcom/keymaster
+	hardware/qcom/media"
 
 . `dirname $0`/fork-lib.sh
 
