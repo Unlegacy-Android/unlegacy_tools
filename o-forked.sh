df13fcbb01ae0bcad2474d19283f79d73b716153
D. Andrei Măceș <andrei@unlegacy-android.org>
2018-08-07 09:32:35 +0300

    o-forked: Track oreo-m4-s11-release
    Change-Id: Iac66ae167de5525207b0dd04943d43d5ce208ff7
    Signed-off-by: D. Andrei Măceș <andrei@unlegacy-android.org>

--- o-forked.sh
+++ o-forked.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-FALLBR=aosp-8.0; BRANCH=aosp-8.1; TRACK=oreo-m4-s8-release
+FALLBR=aosp-8.0; BRANCH=aosp-8.1; TRACK=oreo-m4-s11-release
 
 AOSP_FORKS="build/make build/soong bionic
 	hardware/interfaces hardware/ril
