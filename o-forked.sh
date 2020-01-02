ecb0623929d1e547488092a85811259207fd1d20
D. Andrei Măceș <andrei@unlegacy-android.org>
2020-01-02 12:56:20 +0200

    o/p-forked: Fork hardware/broadcom/libbt
    Change-Id: I4e6c04790886dcc2bdfeccb1275ce9c6fa1edbff
    Signed-off-by: D. Andrei Măceș <andrei@unlegacy-android.org>

--- o-forked.sh
+++ o-forked.sh
@@ -5,8 +5,8 @@ FALLBR=aosp-8.0; BRANCH=aosp-8.1; TRACK=oreo-m8-release
 AOSP_FORKS="build/make build/soong bionic hardware/ril
 	system/bt system/core system/nfc system/sepolicy
 	frameworks/av frameworks/base frameworks/native
-	hardware/broadcom/wlan hardware/qcom/audio
-	hardware/qcom/bt hardware/qcom/display
+	hardware/broadcom/wlan hardware/broadcom/libbt
+	hardware/qcom/audio hardware/qcom/bt hardware/qcom/display
 	hardware/qcom/gps hardware/qcom/keymaster
 	hardware/qcom/media hardware/qcom/power"
 
--- p-forked.sh
+++ p-forked.sh
@@ -5,8 +5,8 @@ BRANCH=aosp-9.0; TRACK=pie-qpr3-release
 AOSP_FORKS="build/make build/soong bionic hardware/ril
 	external/perfetto system/bt system/core system/sepolicy
 	frameworks/av frameworks/base frameworks/native
-	hardware/broadcom/wlan hardware/qcom/audio
-	hardware/qcom/bt hardware/qcom/display
+	hardware/broadcom/wlan hardware/broadcom/libbt
+	hardware/qcom/audio hardware/qcom/bt hardware/qcom/display
 	hardware/qcom/gps hardware/qcom/keymaster
 	hardware/qcom/media hardware/qcom/power"
 
